0aa8afae72ea1b30d6f44eff5885b6abd68e2b5e
mebyus <mebyus@gmail.com>
2023-06-04 16:30:12 +0300

    sketch: parse token listing

--- list/list.go
+++ list/list.go
@@ -1,6 +1,8 @@
 package list
 
 import (
+	"bufio"
+	"fmt"
 	"io"
 
 	"codeberg.org/mebyus/hx/lexer"
@@ -32,4 +34,27 @@ func put(w io.Writer, tok token.Token) error {
 func newline(w io.Writer) error {
 	_, err := w.Write([]byte("\n"))
 	return err
+}
+
+func Parse(r io.Reader) ([]token.Token, error) {
+	sc := bufio.NewScanner(r)
+	var toks []token.Token
+	i := 0
+	for sc.Scan() {
+		i++
+		line := sc.Text()
+		if line == "" {
+			continue
+		}
+		tok, err := token.Parse(line)
+		if err != nil {
+			return nil, fmt.Errorf("line %d: %w", i, err)
+		}
+		toks = append(toks, tok)
+	}
+	err := sc.Err()
+	if err != nil {
+		return nil, err
+	}
+	return toks, nil
 }
---
+++ token/parse.go
@@ -0,0 +1,5 @@
+package token
+
+func Parse(line string) (Token, error) {
+	return Token{}, nil
+}
